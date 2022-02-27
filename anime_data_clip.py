062d621b86cdbf8b2150fda0f56a61ba9e530003
Lewis Jang <lewis.aedes@gmail.com>
2022-02-27 17:34:40 +0800

    3 frame marge in training, not clip

--- anime_data_clip.py
+++ anime_data_clip.py
@@ -33,55 +33,27 @@ if __name__ == '__main__':
             img = np.array(sct.grab(monitor))
             img_s = cv2.resize(img[:], (480, 270))
             img_s = cv2.cvtColor(img_s, cv2.COLOR_RGBA2RGB)
+            images_to_save = img_s[:]
 
-            """
-            img_hsv = img_s[:]
-            img_hsv = cv2.cvtColor(img_hsv, cv2.COLOR_RGB2HSV)
-            img_r, img_g, img_b = [img_hsv[:]] * 3
+            mouse.hook(mouse_events.append)
+            keyboard.start_recording()
 
-            img_r = cv2.inRange(img_r, np.array([50, 0, 0]), np.array([255, 200, 255]))
-            img_g = cv2.inRange(img_g, np.array([0, 50, 0]), np.array([200, 255, 200]))
-            img_b = cv2.inRange(img_b, np.array([0, 0, 50]), np.array([200, 200, 255]))
+            time.sleep(0.01)
 
-            img_r = cv2.bitwise_and(img_s, img_s, mask=img_r)
-            img_g = cv2.bitwise_and(img_s, img_s, mask=img_g)
-            img_b = cv2.bitwise_and(img_s, img_s, mask=img_b)
+            mouse.unhook(mouse_events.append)
+            keyboard_events = keyboard.stop_recording()
 
-            img_r = cv2.cvtColor(img_r, cv2.COLOR_RGB2GRAY)
-            img_g = cv2.cvtColor(img_g, cv2.COLOR_RGB2GRAY)
-            img_b = cv2.cvtColor(img_b, cv2.COLOR_RGB2GRAY)
-            img_s = cv2.cvtColor(img_s, cv2.COLOR_RGB2GRAY)
+            labels = [unit.mouse_event_process(mouse_events[-30:]), unit.keyboard_event_process(keyboard_events[-30:])]
+            xs.append(images_to_save)
+            ys.append(labels)
+            cv2.imshow("save", images_to_save)
+            print(labels, "\n")
 
-            view_image = np.concatenate((img_s, img_r, img_g, img_b), axis=0)
-            """
-            view_image = img_s[:]
-
-            if i == 0:
-                mouse.hook(mouse_events.append)
-                keyboard.start_recording()
-                images_to_save = view_image[:]
-            else:
-                images_to_save = np.concatenate((images_to_save, view_image), axis=1)
-
-            if i == 2:
-                mouse.unhook(mouse_events.append)
-                keyboard_events = keyboard.stop_recording()
-
-                labels = [unit.mouse_event_process(mouse_events[-30:]), unit.keyboard_event_process(keyboard_events[-30:])]
-                xs.append(images_to_save)
-                ys.append(labels)
-                cv2.imshow("save", images_to_save)
-                print(labels, "\n")
-
-                i = 0
-
-                if cv2.waitKey(25) & 0xFF == ord("q"):
-                    cv2.destroyAllWindows()
-                    np.save(config.save_dataset_content, xs)
-                    np.save(config.save_dataset_label, ys)
-                    break
-            else:
-                i += 1
+            if cv2.waitKey(25) & 0xFF == ord("q"):
+                cv2.destroyAllWindows()
+                np.save(config.save_dataset_content, xs)
+                np.save(config.save_dataset_label, ys)
+                break
 
 
 
